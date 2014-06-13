ce4d5dc57973bf4a7c94f9ae27ddc36d7ff7aa63
Artiom Basenko <demi.log@gmail.com>
2014-06-13 12:42:55 +0400

    Basic functionality.

--- hive/urls.py
+++ hive/urls.py
@@ -7,6 +7,11 @@ urlpatterns = patterns(
     '',
     # Landing
     url(r'^$', views.index, name='index'),
+    # Device list
+    url(r'^grunts/', views.grunts, name='grunts'),
+    # url(r'^sms/', views.sms, name='sms'),
+    # Additional
+    url(r'^info/', views.info, name='info'),
     # Auth
     # url(r'^login/', views.login, name='login'),
     # url(r'^register/', views.register, name='register'),
--- hive/views.py
+++ hive/views.py
@@ -1,12 +1,30 @@
 from django.shortcuts import render
 
-                               ################
-                               # Landing page #
-                               ################
+from utils.scanner import Scanner
+
+################
+# Landing page #
+################
+
 
 def index(request):
     """Display landing page"""
-    # if request.user.is_authenticated():
-    #     return redirect('profile')
-
     return render(request, 'home.html')
+
+#####################
+# Working with hive #
+#####################
+
+
+def grunts(request):
+    grunts = Scanner().scan()
+    return render(request, 'grunts.html', {'grunts': grunts})
+
+
+##############
+# Additional #
+##############
+
+
+def info(request):
+    return render(request, 'about.html')
